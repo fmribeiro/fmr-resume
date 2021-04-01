f4ad44824da96bdded9da50ded429017bd12b013
fmribeiro <fmribeiro21@gmail.com>
2021-04-01 19:39:40 -0300

    atualizacao do curriculo, ajustes bibliotecas

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('css:compile', function() {
       outputStyle: 'expanded'
     }).on('error', sass.logError))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      // browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(header(banner, {
@@ -66,7 +66,7 @@ gulp.task('css:compile', function() {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
+gulp.task('css:minify', gulp.series('css:compile'), function() {
   return gulp.src([
       './css/*.css',
       '!./css/*.min.css'
@@ -80,7 +80,7 @@ gulp.task('css:minify', ['css:compile'], function() {
 });
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
 gulp.task('js:minify', function() {
@@ -100,24 +100,24 @@ gulp.task('js:minify', function() {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor']);
+gulp.task('default', gulp.series('css', 'js', 'vendor'));
 
 // Configure the browserSync task
-gulp.task('browserSync', [], function() {
+gulp.task('browserSync', gulp.series(function() {
   // .init starts the server
   browserSync.init({
     server: "./",
     port: process.env.PORT || 3000
   });
-});
+}));
 
 
 // Serve task
-gulp.task('serve', ['css', 'js', 'browserSync'], function() {
+gulp.task('serve', gulp.series('css', 'js', 'browserSync', function() {
   gulp.watch('./scss/*.scss', ['css']);
   gulp.watch('./js/*.js', ['js']);
   gulp.watch('./*.html', browserSync.reload);
-});
+}));
